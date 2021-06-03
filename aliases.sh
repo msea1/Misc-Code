42b52f66a3cab2ea1adc21c8a0dc50768415e5fa
Matthew Carruth <carruthm@gmail.com>
2021-06-03 11:45:54 -0700

    stop mcafee

--- aliases.sh
+++ aliases.sh
@@ -32,7 +32,7 @@ alias qemu='make && ./provision.sh -X -i images -q -S -c && cinderblock -i provi
 alias root="sudo su -"
 alias sorry='sudo $(fc -ln -1)'
 alias sudo_pw='cat ~/.sudo_pw | xsel -ib'
-alias update='sudo apt update && sudo apt -y upgrade && sudo apt dist-upgrade && sudo apt autoremove'
+alias update='sudo /opt/McAfee/agent/bin/macmnsvc stop && sudo apt update && sudo apt -y upgrade && sudo apt dist-upgrade && sudo apt autoremove'
 alias usb_f5='sudo usbmuxd -u -U usbmux'
 alias vauth='vault auth -method=ldap username=$USER'
 alias vssh='vault ssh -role otp_key_role'
