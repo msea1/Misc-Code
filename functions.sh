159118796bca48c94c43cf1e7f6b0f3bed1b51f1
Matthew Carruth <carruthm@gmail.com>
2022-02-21 14:11:00 -0800

    most host

--- functions.sh
+++ functions.sh
@@ -108,7 +108,7 @@ gemini_tests() {
   gemini
   echo -e 'cd /code\n./test-all.sh > /tmp/tests.txt' > /tmp/tests.sh
   echo -e "grep -Eo '(.*):test.*\.\.\.\.\.   FAILURE' /tmp/tests.txt | sort | uniq -c | awk '{print \$4\": \"\$2}' && grep -Eo '\.\.\.\.\.   SUCCESS' /tmp/tests.txt | sort | uniq -c | awk '{print \$3\": \"\$1}'\nexit" >> /tmp/tests.sh
-  sudo docker run --rm -v ~/Code/gemini:/code -v /tmp/tests.sh:/tests.sh pants-build bash tests.sh
+  sudo docker run --net host --rm -v ~/Code/gemini:/code -v /tmp/tests.sh:/tests.sh pants-build bash tests.sh
   cd -
 }
 
